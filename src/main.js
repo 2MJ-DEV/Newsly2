bff8cf449fd96b1409fe777a8ead9457bb8b8e7d
Jules-2MJ <146131402+2MJ-DEV@users.noreply.github.com>
2025-05-11 13:17:48 +0100

    Finalisation de la waitlist en ajoutant les images en bas et en pofinant l'interface et l'adapter a tout les support

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import './style.css'
+
+
+// Interdiction d'inspecter le code
+// document.addEventListener('contextmenu', function (e) {
+//     e.preventDefault();
+// });
+
+// document.addEventListener('keydown', function (e) {
+//     // F12
+//     if (e.key === "F12") {
+//         e.preventDefault();
+//     }
+//     // Ctrl+Shift+I / Ctrl+Shift+J / Ctrl+U
+//     if ((e.ctrlKey && e.shiftKey && (e.key === "I" || e.key === "J")) ||
+//         (e.ctrlKey && e.key === "U")) {
+//         e.preventDefault();
+//     }
+// });
+
+
